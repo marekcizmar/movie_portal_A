e0385588e1b8b9258a74bba8048deae7817d6292
0ndrik <ondrejsvarny1@gmail.com>
2023-05-25 15:13:25 +0200

    Update Content.vue

--- client/src/components/Content.vue
+++ client/src/components/Content.vue
@@ -1,9 +1,15 @@
 <template>
     <div class="mt-24 container mx-auto flex justify-center">
-        <div>
+        <div class="relative z-10">
             <div class="bg-blue-400 w-48 text-center py-3 rounded-t-lg text-white">NEWEST</div>
             <div class="border-blue-400 border-4 bg-blue-400 border-15px shadow-xl">
                 <iframe width="840" height="472.5" src="https://www.youtube.com/embed/qEVUtrk8_B4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+            </div>
+        </div>
+        <div class="absolute z-0">
+            <div class="bg-blue-900 w-48 text-center py-3 rounded-t-lg text-white ml-48">TOP RATED</div>
+            <div class="border-blue-900 border-4 bg-blue-900 border-15px2 shadow-xl">
+                <iframe width="840" height="472" src="https://www.youtube.com/embed/d9MyW72ELq0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
             </div>
         </div>
     </div>
