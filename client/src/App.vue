014e3acd9d5e239cab34ee04528a1c3d8b9cafc6
Marek Čižmár <marekcizmar@outlook.sk>
2023-05-25 14:12:07 +0200

    header app

--- client/src/App.vue
+++ client/src/App.vue
@@ -13,4 +13,25 @@ import FilmContainer from './components/FilmContainer.vue';
   @tailwind base;
   @tailwind components;
   @tailwind utilities;
+
+  @layer components{
+    .nav_a {
+      @apply mr-4 hover:text-blue-600 hover:bg-slate-50 transition duration-150 ease-in-out py-3 px-1 rounded-xl
+    }
+  }
+
+  * {
+    font-family: 'Inter', sans-serif;
+  }
+
+  .border-left-1 {
+    border-left: 1px solid rgb(225, 225, 225);
+  }
+
+  .py-2-5 {
+    padding-top: 0.625rem;
+    padding-bottom: 0.625rem;
+  }
+
+
 </style>
--- client/src/components/Header.vue
+++ client/src/components/Header.vue
@@ -1,10 +1,22 @@
-
-
 <template>
-    <div>
-        <nav class="container mx-auto">
-            <h1>NÁZOV</h1>
-            <i class="fa-solid fa-magnifying-glass"></i>
+    <div class="shadow-lg">
+        <nav class="container mx-auto p-5">
+            <main class="flex justify-between">
+                <div class="flex content-center">
+                    <h1 class="text-xl py-2-5 px-1"><a href="">NÁZOV</a></h1>
+                    <div class="border-left-1 ml-10 mr-10 py-2 px-1"></div>
+                    <h1 class="nav_a animation"><a href="" class="px-4">Home</a></h1>
+                    <h1 class="nav_a animation"><a href="" class="px-4">Movies</a></h1>
+                    <h1 class="nav_a animation"><a href="" class="px-4">Genres</a></h1>
+                </div>
+                <div>
+                    <input type="text" placeholder="Quick search" class="border border-slate-400 rounded-l-xl p-2 pr-16">
+                    <button class="bg-blue-400 border border-blue-400 hover:bg-blue-300 hover:border-blue-300 transition duration-150 ease-in-out rounded-r-xl py-2 px-12 mr-14">
+                        <i class="fa-solid fa-magnifying-glass" style="color: white"></i>
+                    </button>
+                    <button class="text-slate-500 hover:bg-slate-50 hover:text-blue-500 rounded-xl p-3 transition duration-150 ease-in-out"><i class="fa-solid fa-moon fa-xl"></i></button>
+                </div>
+            </main>
         </nav>
     </div>
 </template>
