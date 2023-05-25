b3e1a715d2577928dfe7df1fefffd911b8137dab
0ndrik <ondrejsvarny1@gmail.com>
2023-05-25 14:09:29 +0200

    content

---
+++ client/src/components/Content.vue
@@ -0,0 +1,6 @@
+<template>
+    <div class="rounded-md absolute bg-blue-400 text-white p-3 pl-12 pr-12 ml-8">NEWEST</div>
+    <div class="absolute border-blue-400 border-4 bg-blue-400 m-8 border-15px">
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/qEVUtrk8_B4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+    </div>
+  </template>
