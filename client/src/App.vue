8f4b04ebeb60be1079bbfda67ebd2a4c61393d74
mhilgner <119048141+jaKusmara@users.noreply.github.com>
2023-05-25 08:39:20 +0200

    tailwind

--- client/src/App.vue
+++ client/src/App.vue
@@ -5,8 +5,11 @@ import HelloWorld from './components/HelloWorld.vue'
 <template>
   <div>
     <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
+      <img src="/vite.svg" class="logo " alt="Vite logo" />
     </a>
+    <p class="text-red-600 bg-orange-900">
+sdfdsfbdsfsdhfsdhf
+    </p>
     <a href="https://vuejs.org/" target="_blank">
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
@@ -14,17 +17,8 @@ import HelloWorld from './components/HelloWorld.vue'
   <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+<style>
+@tailwind base;
+@tailwind components;
+@tailwind utilities;
 </style>
