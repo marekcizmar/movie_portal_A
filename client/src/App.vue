7e41bc479e12ad5a3255c7848f7c978bd2ea935d
Marek Čižmár <marekcizmar@outlook.sk>
2023-05-25 09:44:28 +0200

    kit

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,24 +1,13 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue'
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo " alt="Vite logo" />
-    </a>
-    <p class="text-red-600 bg-orange-900">
-sdfdsfbdsfsdhfsdhf
-    </p>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <Header/>
 </template>
 
 <style>
-@tailwind base;
-@tailwind components;
-@tailwind utilities;
+  @tailwind base;
+  @tailwind components;
+  @tailwind utilities;
 </style>
---
+++ client/src/components/Header.vue
@@ -0,0 +1,10 @@
+
+
+<template>
+    <div>
+        <nav class="container mx-auto">
+            <h1>NÁZOV</h1>
+            <i class="fa-solid fa-magnifying-glass"></i>
+        </nav>
+    </div>
+</template>
