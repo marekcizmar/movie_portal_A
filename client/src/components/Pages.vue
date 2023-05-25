69ae49fc77247207db925b625cfa79aa27e1eef3
matejmadzin <matejmadzin@gmail.com>
2023-05-25 14:04:14 +0200

    footer/pages

---
+++ client/src/components/Pages.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <div class="flex justify-center space-x-1">
+            <button class="w-8 h-9 drop-shadow-xl rounded-md hover:bg-cyan-400 border-width:2px border-black shadow  dark:bg-gray-800">
+                <p class="text-base hover:text-white">1</p>
+            </button>
+            <button class=" w-8 h-9 drop-shadow-xl rounded-md hover:bg-cyan-400 border-width:2px border-black shadow  dark:bg-gray-800">
+                <p class="text-base hover:text-white">2</p>
+            </button>
+            <button class=" w-8 h-9 drop-shadow-xl rounded-md hover:bg-cyan-400 border-width:2px border-black shadow  dark:bg-gray-800">
+                <p class="text-base hover:text-white">3</p>
+            </button>
+            <button class=" w-8 h-9 drop-shadow-xl rounded-md hover:bg-cyan-400 border-width:2px border-black shadow  dark:bg-gray-800">
+                <p class="text-base hover:text-white">></p>
+            </button>
+        </div>
+    </div>
+</template>
