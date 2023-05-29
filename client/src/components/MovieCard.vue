2bb58bffc4e237c53996555b6593842db906a2bb
JanKecer <119047988+JanKecer@users.noreply.github.com>
2023-05-29 09:47:07 +0200

    delete button success

--- client/src/components/MovieCard.vue
+++ client/src/components/MovieCard.vue
@@ -1,7 +1,39 @@
 <script>
 
-export default{
-    props: ['moviesData'],
+export default {
+    data(){
+        return{
+            moviesData: []
+        }
+    },
+    methods: {
+        async getData(){
+            const res = await fetch('http://localhost:5000/movie')
+            const finalRes = await res.json()
+            this.moviesData = finalRes
+            console.log(finalRes)
+        },
+        async deleteFilm(movie) {
+    try {
+      const response = await fetch(`http://localhost:5000/movie/${movie.id}`, {
+        method: 'DELETE'
+      });
+      if (response.ok) {
+        
+        this.moviesData = this.moviesData.filter((m) => m.id !== movie.id);
+      } else {
+       
+        console.error('Failed to delete film');
+      }
+    } catch (error) {
+      console.error('An error occurred:', error);
+    }
+  }
+    },
+    mounted(){
+        this.getData()
+    },
+  
 };
 
 </script>
@@ -23,7 +55,7 @@ export default{
             </div> 
         </span>
         <span class="btn-container inline-flex w-48 justify-between" >
-            <button class="btn-remove-movie bg-blue-400 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-500 rounded">Remove</button>
+            <button class="btn-remove-movie bg-blue-400 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-500 rounded" @click="deleteFilm(movie)">Remove{{ movie.id }}</button>
             <button class="btn-update-movie bg-blue-400 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-500 rounded">Update</button>
         </span>
     </div>
