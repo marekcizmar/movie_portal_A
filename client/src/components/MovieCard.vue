9ddca90eb78674a8676145a99c222e293549ec67
Marek Čižmár <marekcizmar@outlook.sk>
2023-05-29 09:15:01 +0200

    Searchbar adjustments & movie cards sizing changes

--- client/src/components/MovieCard.vue
+++ client/src/components/MovieCard.vue
@@ -1,5 +1,4 @@
 <script>
-
 export default {
     data(){
         return{
@@ -18,29 +17,23 @@ export default {
         this.getData()
     }
 }
-
-
 </script>
 
 <template>
-    <div class="movie-container w-48 h-80 m-4" v-for="movie of moviesData" >
+    <div class="movie-container m-4" v-for="movie of moviesData" >
+        <img class="shadow-lg w-48 h-72" :src="movie.image" alt="">
+        <span class="movie-footer mt-3 flex justify-center gap-8">
+            <div class="movie-relese-date self-end ">
+                {{ movie.year }}
+            </div>
 
-        <span class="movie-preview h-64">
-            <img class="shadow-lg" :src="movie.image" alt="">
-        </span>
+            <div class="movie-length table-cell self-end">
+                {{ movie.length }}min
+            </div>
 
-        <span class="movie-footer w-48 h-9 flex flex-nowrap justify-center gap-8">
-                <div class="movie-relese-date self-end ">
-                    {{ movie.year }}
-                </div>
-
-                <div class="movie-length table-cell self-end">
-                    {{ movie.length }}min
-                </div>
-
-                <div class="movie-resolution table-cell self-end">
-                    {{ movie.quality }}
-                </div> 
+            <div class="movie-resolution table-cell self-end">
+                {{ movie.quality }}
+            </div> 
         </span>
     </div>
 </template>
--- client/src/components/Searchbar.vue
+++ client/src/components/Searchbar.vue
@@ -1,5 +1,4 @@
 <script>
-
 export default {
     data(){
         return{
@@ -18,52 +17,29 @@ export default {
         this.getDataGeners()
     }
 }
-
-
 </script>
 
 <template>
-  <div class="search-bar-container flex justify-center mt-24 mx-24">
-    <input type="text" placeholder="Search for a movie..." class="border border-slate-400 rounded-l-xl p-5 w-full">
-    <button type="submit" class=" bg-blue-400 px-16 rounded-r-xl">
-      <i class="fa fa-search text-white fa-lg"></i>   
-    </button>
+  <div class="container mx-auto mt-24">
+    <div class=" flex justify-center">
+      <input type="text" placeholder="Search for a movie..." class="border border-slate-400 rounded-l-xl p-5 w-full">
+      <button type="submit" class=" bg-blue-400 px-16 rounded-r-xl">
+        <i class="fa fa-search text-white fa-lg"></i>   
+      </button>
+    </div>
+    <form action="" class="flex-row flex mx-24 justify-between">
+      <select  id="genres" class="cursor-pointer hover:bg-slate-100 transition h-10 w-32 rounded-md m-10 text-slate-500 text-left border-none shadow-xl outline-none flex-row">
+        <option value="0">Main</option>
+        <option  v-for="genre in moviesGeners" value="@{{ genre.id }}">{{ genre.title }}</option>
+      </select>
+      <div class="slidecontainer">
+        <p class="absolute py-11 text-slate-400 ">Year:</p>
+        <input type="range" min="2015" max="2018" value="2015" class="slider absolute m-12 w-40" id="myRange">
+        <input type="range" min="2015" max="2018" value="2018" class="slider relative m-12 w-40" id="myRange2">
+        <br>
+        <p class="absolute px-10 -my-10 text-slate-400">2015</p>
+        <p class="absolute px-44 -my-10 text-slate-400 ">2018</p>
+      </div>
+    </form>
   </div>
-<form action="" class="flex-row flex mx-24 justify-between">
-<select  id="genres" class="
-cursor-pointer
-hover:bg-slate-100
-transition
-h-10
-w-32 
-rounded-md  
-m-10
-text-slate-500
-text-left
-border-none
-shadow-xl
-outline-none 
-flex-row 
-">
-<option value="0">Main</option>
-  
-  <option  v-for="genre in moviesGeners" value="@{{ genre.id }}">{{ genre.title }}</option>
-  
-
-</select>
-<div class="slidecontainer">
-  <p class="absolute py-11 text-slate-400 ">Year:</p>
-  <input type="range" min="2015" max="2018" value="2015" class="slider absolute m-12 w-40" id="myRange">
-  <input type="range" min="2015" max="2018" value="2018" class="slider relative m-12 w-40" id="myRange2">
-  <br>
-  <p class="absolute px-10 -my-10 text-slate-400">2015</p>
-  <p class="absolute px-44 -my-10 text-slate-400 ">2018</p>
-</div>
-</form>
 </template>
-
-<style scoped>
-
-
-
-</style>
