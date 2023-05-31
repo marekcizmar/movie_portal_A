a6565e7b7401de003f189a7047be9d1ff2b4b629
Marek Čižmár <marekcizmar@outlook.sk>
2023-05-31 09:12:48 +0200

    footer changes

--- client/src/components/Footer.vue
+++ client/src/components/Footer.vue
@@ -1,9 +1,7 @@
 <template>
     <div>
-        <footer class="bg-white rounded-lg shadow m-4 dark:bg-gray-800">
-            <div class="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center place-content-center">
-                <span class="flex items-center justify-center text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 Nazov tímu. All Rights Reserved.</span>
-            </div>
+        <footer class="shadow-2xl p-8 text-center">
+            <span class="text-gray-500">© 2023 FilmFusion</span>
         </footer>
     </div>
 </template>
